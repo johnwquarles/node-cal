db7451f35fd42f96dcb21cbe41415afd358f5c2f
johnwquarles <john.w.quarles@gmail.com>
2015-07-24 14:25:32 -0500

    broken down further; makecal now just handles the initial logic depending on number of arguments, writeMonth & writeYear are what makecal was

--- node-cal.js
+++ node-cal.js
@@ -1,15 +1,6 @@
 #!/usr/bin/env node
 
 var args = process.argv.slice(2);
-var makecal = require('./lib/makecal');
+var cal = require('./lib/makecal');
 
-if (args.length === 2) {
-  if (args[0] < 1 || args[0] > 12 || args[1] < 1754) {console.log("Invalid Input!"); return}
-  makecal.writeMonth(args[0], args[1]);}
-else if (args.length === 1) {
-  if (args[0] < 1754) {console.log("Invalid input!"); return}
-  makecal.writeYear(args[0]);}
-else {
-  var d = new Date();
-  makecal.writeMonth(d.getMonth() + 1, d.getFullYear());
-}
+cal.makeCal(args);
