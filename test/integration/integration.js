071edcd53e344962a0ac59a070ebfa8cf0e2918c
johnwquarles <john.w.quarles@gmail.com>
2015-07-24 21:53:55 -0500

    integration for no arguments passed in

--- test/integration/integration.js
+++ test/integration/integration.js
@@ -14,11 +14,15 @@ describe('CLI', function () {
       done();
     });
   });
-  it('shouldn\'t give the error message when the input is ok', function (done) {
+  it('shouldn\'t give the error message when the input is ok, either with a year, month and year, or no input (current date)', function (done) {
     cp.execFile('./node-cal.js', ["2000"],function (err, stdout) {
       stdout.should.not.equal('Invalid input!\n');
     });
     cp.execFile('./node-cal.js', ["00009", "2009"],function (err, stdout) {
+      stdout.should.not.equal('Invalid input!\n');
+      done();
+    });
+    cp.execFile('./node-cal.js',function (err, stdout) {
       stdout.should.not.equal('Invalid input!\n');
       done();
     });
